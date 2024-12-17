fa81b1497932b770444249ce30d7c451abfbf2b0
tauzn-clock <zhongdaoxin1999@gmail.com>
2024-12-17 00:30:59 +0000

    downgrae model size

--- src/model.py
+++ src/model.py
@@ -16,19 +16,19 @@ class ModelConfig():
             self.depths = [2, 2, 18, 2]
             self.num_heads = [4, 8, 16, 32]
             self.in_channels = [128, 256, 512, 1024]
-            self.swinv2_pretrained_path = "microsoft/swinv2-base-patch4-window12to24-192to384-22kto1k-ft"
+            self.swinv2_pretrained_path = "microsoft/swinv2-base-patch4-window8-256"
         elif version[:-2] == 'large':
             self.embed_dim = 192
             self.depths = [2, 2, 18, 2]
             self.num_heads = [6, 12, 24, 48]
             self.in_channels = [192, 384, 768, 1536]
-            self.swinv2_pretrained_path = "microsoft/swinv2-large-patch4-window12to24-192to384-22kto1k-ft"
+            self.swinv2_pretrained_path = "microsoft/swinv2-large-patch4-window12-192-22k"
         elif version[:-2] == 'tiny':
             self.embed_dim = 96
             self.depths = [2, 2, 6, 2]
             self.num_heads = [3, 6, 12, 24]
             self.in_channels = [96, 192, 384, 768]
-            self.swinv2_pretrained_path = "microsoft/swinv2-tiny-patch4-window16-256"
+            self.swinv2_pretrained_path = "microsoft/swinv2-tiny-patch4-window8-256"
 
         print(self.swinv2_pretrained_path)
             
