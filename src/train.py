da680a5b13ff13ed7ec5e3b6bace65d78569d139
tauzn-clock <zhongdaoxin1999@gmail.com>
2025-01-14 02:04:36 +0000

    blur normal

--- src/train.py
+++ src/train.py
@@ -12,7 +12,7 @@ import os
 import torch.distributed as dist
 from torch.nn.parallel import DistributedDataParallel as DDP
 import torch.multiprocessing as mp
-from torchvision import transforms
+from torchvision.transforms import GaussianBlur
 
 from model import Model, ModelConfig
 from dataloader.BaseDataloader import BaseImageDataset
@@ -77,6 +77,7 @@ def main(local_rank, world_size):
     silog_criterion = silog_loss(variance_focus=args.var_focus).to(local_rank)
     dn_to_distance = DN_to_distance(args.batch_size, args.height, args.width).to(local_rank)
     normal_estimation = Depth2Normal(local_rank)
+    gaussian_blur = GaussianBlur(kernel_size=args.gaussian_blur_kernel, sigma=(args.gaussian_blur_kernel-1)/6)
 
     for epoch in range(args.total_epoch):
         model.train()
@@ -84,18 +85,20 @@ def main(local_rank, world_size):
         loop = tqdm.tqdm(train_dataloader, desc=f"Epoch {epoch+1}", unit="batch")
         for itr, x in enumerate(loop):
             optimizer.zero_grad()
-            for k in x.keys():
-                x[k] = x[k].to(local_rank)
-            if x["depth_values"].shape[0] != args.batch_size: continue # Hacky solution to deal with batch size issue
-            
-            # Estimate GT normal and distance
+            with torch.no_grad():
+                for k in x.keys():
+                    x[k] = x[k].to(local_rank)
+                if x["depth_values"].shape[0] != args.batch_size: continue # Hacky solution to deal with batch size issue
+                
+                # Estimate GT normal and distance
 
-            depth_gt = x["depth_values"] #Unit: m
-            normal_gt, x["mask"] = normal_estimation(depth_gt, x["camera_intrinsics"], x["mask"], args.normal_blur) # Intrinsic needs to be in mm, ideally change depth_gt to mm for consistency, skip for speed
-            normal_gt = F.normalize(normal_gt, dim=1, p=2) #Unit: none, normalised
-            dist_gt = dn_to_distance(depth_gt, normal_gt, x["camera_intrinsics_inverted"]) #Camera intrinsic needs to be in mm, but dist_gt is in m, probably dont need to scale depth_gt but just to be safe
-            normal_gt = normal_gt.detach()
-            dist_gt = dist_gt.detach()
+                depth_gt = x["depth_values"] #Unit: m
+                normal_gt, x["mask"] = normal_estimation(depth_gt, x["camera_intrinsics"], x["mask"], 1.0) # Intrinsic needs to be in mm, ideally change depth_gt to mm for consistency, skip for speed
+                for b in range(len(normal_gt)):
+                    normal_gt[b] = gaussian_blur(normal_gt[b])
+                normal_gt = normal_gt * x["mask"]
+                normal_gt = F.normalize(normal_gt, dim=1, p=2) #Unit: none, normalised
+                dist_gt = dn_to_distance(depth_gt, normal_gt, x["camera_intrinsics_inverted"]) #Camera intrinsic needs to be in mm, but dist_gt is in m, probably dont need to scale depth_gt but just to be safe
 
             # CutMix
             if args.cutmix:
